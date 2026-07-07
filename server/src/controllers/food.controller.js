c510ba54e8c022550edea72b2eff1c6049909f6d
GoutamChoudharyDev <rc.goutam.choudhary@gmail.com>
2026-07-07 12:33:52 +0530

    fetch food api's

--- server/src/controllers/food.controller.js
+++ server/src/controllers/food.controller.js
@@ -1,6 +1,6 @@
 import { Food } from "../models/food.model.js";
 
-// addFood controller
+// addFood controller (private)
 const addFood = async (req, res) => {
     try {
         // get id 
@@ -46,7 +46,65 @@ const addFood = async (req, res) => {
     }
 }
 
+// get all food (public)
+const getAllFood = async (req, res) => {
+    try {
+        // get all food
+        const foodItems = await Food.find().populate("createdBy", "fullName email role");
+
+        // validation
+        if (foodItems.length === 0) {
+            return res.status(200).json({
+                message: "No food items found",
+                foodItems: []
+            })
+        }
+
+        // return response
+        return res.status(200).json({
+            message: "Fetching all food items successfully",
+            foodItems
+        })
+
+    } catch (error) {
+        console.error("Internal server error during getting food items: ", error);
+        return res.status(500).json({
+            message: "Internal server error during getting food itmes"
+        })
+    }
+}
+
+// get single food (public)
+const getSingleFood = async (req, res) => {
+    try {
+        // get id from params
+        const { id } = req.params;
+        console.log("food id", id);
+
+        const singleFood = await Food.findById(id).populate("createdBy", "fullName email role");
+
+        if (!singleFood) {
+            return res.status(404).json({
+                message: "Food not found"
+            })
+        }
+
+        // return response
+        return res.status(200).json({
+            message: "Fetch food successfully",
+            singleFood
+        })
+    } catch (error) {
+        console.error("Internal server error during getting single food items: ", error);
+        return res.status(500).json({
+            message: "Internal server error during getting single food itmes"
+        })
+    }
+}
+
 // export
 export {
     addFood,
+    getAllFood,
+    getSingleFood
 }
--- server/src/routes/food.route.js
+++ server/src/routes/food.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { isAdmin, isAuth } from "../middlewares/user.middleware.js";
-import { addFood } from "../controllers/food.controller.js";
+import { addFood, getAllFood, getSingleFood } from "../controllers/food.controller.js";
 
 // router inizialize
 const router = Router();
@@ -8,6 +8,11 @@ const router = Router();
 // food api's (Private)
 router.post("/add", isAuth, isAdmin, addFood);
 
+// food api's (Public)
+router.get("/get-all", isAuth, getAllFood);
+
+// single food 
+router.get("/:id", isAuth, getSingleFood);
 
 // exports
 export default router;
